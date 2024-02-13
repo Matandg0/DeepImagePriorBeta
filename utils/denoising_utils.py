bc8837265c196b98fa89ea8aaab3890e4b49e234
Hugo Dufresne <94015531+Matandg0@users.noreply.github.com>
2024-02-13 12:08:55 +0100

    Update denoising_utils.py
    add function noiser with mean

--- utils/denoising_utils.py
+++ utils/denoising_utils.py
@@ -14,3 +14,16 @@ def get_noisy_image(img_np, sigma):
     img_noisy_pil = np_to_pil(img_noisy_np)
 
     return img_noisy_pil, img_noisy_np
+
+
+def get_noisy_image_with_mean(img_np, mean, sigma):
+    """Adds Gaussian noise to an image.
+    
+    Args: 
+        img_np: image, np.array with values from 0 to 1
+        sigma: std of the noise
+    """
+    img_noisy_np = np.clip(img_np + np.random.normal(loc=mean, scale=sigma, size=img_np.shape), 0, 1).astype(np.float32)
+    img_noisy_pil = np_to_pil(img_noisy_np)
+
+    return img_noisy_pil, img_noisy_np
